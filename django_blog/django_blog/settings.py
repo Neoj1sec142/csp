bc797f1622e7ac03514515b494f7e95485395a42
Mark Harmon <neothej1sec@gmail.com>
2022-06-29 00:42:10 -0500

    finished base functionality for blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -128,3 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'mharmondev@gmail.com'
+EMAIL_HOST_PASSWORD = 'scnijbwaghldqzvx'
