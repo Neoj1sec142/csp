b924c1756901aea0caae6faa9f97323f67613f58
Mark Harmon <neothej1sec@gmail.com>
2022-07-14 15:40:37 -0500

    working on models

--- mere/blog/models.py
+++ mere/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class BlogComment(models.Model):
+    post = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
