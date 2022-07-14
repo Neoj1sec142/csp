f19e82133b15776f38e5803b612850c5e1794acb
Mark Harmon <neothej1sec@gmail.com>
2022-07-14 18:52:19 -0500

    set up most of functionality for site just need to create the templates for the front

--- mere/blog/models.py
+++ mere/blog/models.py
@@ -1,12 +1,31 @@
-from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
-class BlogPost(models.Model):
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    catagory = models.CharField(max_length=100, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
 
-class BlogComment(models.Model):
-    post = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
-    content = models.TextField()
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class MerchPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.CharField(max_length=100)
+    catagory = models.CharField(max_length=100)
+    img = models.ImageField(blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('mpost-detail', kwargs={'pk': self.pk})
