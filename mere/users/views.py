9904bf5824722dc6760ca775c902b7c1b1b39ccd
Mark Harmon <neothej1sec@gmail.com>
2022-07-14 20:34:01 -0500

    set up users auth templates

--- mere/users/views.py
+++ mere/users/views.py
@@ -23,8 +23,8 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated.')
-            return redirect('profile')
+            messages.success(request, 'Your account has been updated.')
+            return redirect('users/profile.html')
 
     else:
         u_form = UserUpdateForm(instance=request.user)
