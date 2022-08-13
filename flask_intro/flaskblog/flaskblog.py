e74bde2c499470fc71f7b9b17cdb9881174d9086
Mark Harmon <neothej1sec@gmail.com>
2022-08-13 01:24:43 -0500

    set up registration form and login form

--- flask_intro/flaskblog/flaskblog.py
+++ flask_intro/flaskblog/flaskblog.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6481e35c4a172213fdc7eaafe560ec4f'
 
 posts = [
     {
@@ -26,6 +30,15 @@ def home():
 def about():
     return render_template("about.html", title="About")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
