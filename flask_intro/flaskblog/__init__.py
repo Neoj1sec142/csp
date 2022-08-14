78328837f41c4e6d8f18b62ad70ea3fd3d052844
Mark Harmon <neothej1sec@gmail.com>
2022-08-14 13:02:52 -0500

    working on is_auth check for login redirect

---
+++ flask_intro/flaskblog/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '6481e35c4a172213fdc7eaafe560ec4f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from flaskblog import routes
