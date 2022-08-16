508bfd6193ffbdb9f11b1a5ab0df88b54edf70b0
Mark Harmon <neothej1sec@gmail.com>
2022-08-15 19:19:22 -0500

    finished setting up reset passord email

--- flask_intro/flaskblog/models.py
+++ flask_intro/flaskblog/models.py
@@ -1,5 +1,6 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Ser
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -15,7 +16,17 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-
+    def get_reset_token(self, expires_sec=1800):
+        s = Ser(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    @staticmethod
+    def verify_reset_token(token):
+        s = Ser(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+            return User.query.get(user_id)
+        except:
+            return None
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     
